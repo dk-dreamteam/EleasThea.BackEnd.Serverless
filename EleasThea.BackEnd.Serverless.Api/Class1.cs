1a9abe502015e18efb03732f0be1625618446daf
Dimitris Grevenos <dim.grev@outlook.com>
2020-11-14 20:50:02 +0200

    addding extraaa functionnsss Res and Cook

---
+++ EleasThea.BackEnd.Serverless.Api/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EleasThea.BackEnd.Serverless.Api
+{
+    class Class1
+    {
+    }
+}
---
+++ EleasThea.BackEnd.Serverless.Api/Functions/Gateway/CookingClassFunction.cs
@@ -0,0 +1,48 @@
+using EleasThea.BackEnd.Contracts.InputModels;
+using EleasThea.BackEnd.Serverless.Api.Extentions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace EleasThea.BackEnd.Serverless.Api.Functions.Gateway
+{
+    public static class CookingClassFunction
+    {
+        [FunctionName("CookingClassFunction")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "CookingClass")] HttpRequest req,
+            [Queue("input-messages")] ICollector<string> inputMessagesQueue,
+            ILogger logger)
+        {
+            try
+            {
+                // read body, bind to model.
+                var reservation = await req.GetBodyAsObjectAsync<Reservation>();
+
+                // validate model. if there are errors, return bad request.
+                if (!reservation.IsValid()) return new BadRequestResult();
+
+                // create json serializer settings to include class type in order to deserialize on the other side.
+                var jsonSerializerSettings = new JsonSerializerSettings() { TypeNameHandling = TypeNameHandling.All };
+
+                // enqueue item.
+                inputMessagesQueue.Add(JsonConvert.SerializeObject(reservation, jsonSerializerSettings));
+
+                return new AcceptedResult();
+            }
+            catch (Exception exc)
+            {
+                // log error.
+                logger.LogError($"Message: {exc.Message}, InnerExc: {exc.InnerException.Message}");
+
+                // return internal server error.
+                return new ContentResult { StatusCode = StatusCodes.Status500InternalServerError };
+            }
+        }
+    }
+}
