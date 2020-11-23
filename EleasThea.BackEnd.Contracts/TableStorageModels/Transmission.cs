3c5471c348117d895dd24d404553ad3970c6d0a3
Dimitris Grevenos <dim.grev@outlook.com>
2020-11-23 22:25:01 +0200

    todo ReservationId

--- EleasThea.BackEnd.Contracts/TableStorageModels/Transmission.cs
+++ EleasThea.BackEnd.Contracts/TableStorageModels/Transmission.cs
@@ -6,6 +6,7 @@ namespace EleasThea.BackEnd.Contracts.TableStorageModels
 {
     public class Transmission : TableEntity
     {
+        //todo: ReservationId
         public TransmissionStatus Status { get; set; }
         public bool Opened { get; set; }
         public DateTime? LastOpenedOnUtc { get; set; }
