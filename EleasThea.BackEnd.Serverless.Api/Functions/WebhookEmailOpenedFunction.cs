1d5633bdaab909029aece54c99d9f8b8cfaaa500
Konstantinos Kritikakis <kkritikakis@people-techsolutions.com>
2020-12-25 14:58:55 +0200

    merged webhook gateway and processing into one func

--- EleasThea.BackEnd.Serverless.Api/Functions/WebhookEmailOpenedFunction.cs
+++ EleasThea.BackEnd.Serverless.Api/Functions/WebhookEmailOpenedFunction.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using EleasThea.BackEnd.Contracts.TableStorageModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Table;
 using System.Threading.Tasks;
+using EleasThea.BackEnd.Extentions;
+using System.Web;
 
 namespace EleasThea.BackEnd.Serverless.Services.Functions
 {
@@ -12,14 +16,26 @@ namespace EleasThea.BackEnd.Serverless.Services.Functions
         [FunctionName("WebhookEmailOpenedFunction")]
         public static async Task<IActionResult> RunAsync(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
-            [Queue("email-opened-wbhks")] ICollector<string> emailOpenedTransIdsCollector,
+            [Table("Transmissions")] CloudTable transmissionsTable,
             ILogger log)
         {
-            // get transmission row key from query param.
+            // get transmission row key and email address from query params.
             var transmissionRowKey = req.Query["trk"];
+            var emailAddress = HttpUtility.UrlDecode(req.Query["addr"]);
 
-            // enqueue transmissionId to be processed later.
-            emailOpenedTransIdsCollector.Add(transmissionRowKey);
+            // create a new transmission with an updated opened status.
+            // and assign row and partition keys as the transmission id to updated in the table.
+            var transmissionToUpdate = new Transmission()
+            {
+                Opened = true,
+            }
+            .GeneratePartitionAndRowKeys(emailAddress, transmissionRowKey);
+
+            // insert or merge transmission as read.
+            var insertOrMergeTableOperation = TableOperation.Merge(transmissionToUpdate);
+
+            // execute operation.
+            await transmissionsTable.ExecuteAsync(insertOrMergeTableOperation);
 
             // create response to avoid caching and be able to reregister next opening event.
             req.HttpContext.Response.Headers.Add("Cache-Control", "no-store, max-age=0"); // for HTTP 1.1
